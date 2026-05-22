c20f92676e2f734374c9918d2db8e736e7fdf7d9
K123AsJ0k1 <niila.siilasjoki@gmail.com>
2026-05-22 14:45:36 +0300

    Initial DAS2 script

---
+++ applications/pipelines/rag-coding-assistant/data-analysis-step-2/external_data_analysis.py
@@ -0,0 +1,151 @@
+import sys
+import ray
+import json
+import time
+
+from importlib.metadata import version
+
+from actors.detector import Detector
+from tasks.collector import data_collector
+
+from icebreaker.mlflow.setup import mlflow_setup_client
+from icebreaker.mlflow.use import mlflow_get_or_create_experiment, mlflow_start_run, mlflow_log_metrics, mlflow_change_run_status
+from icebreaker.pararellism.division import division_split_input
+from icebreaker.misc.dict import flatten_nested_dict
+
+def das2_external_data_analysis(
+    job_parameters: any
+):
+    try:  
+        print('Parameters')
+        storage_parameters = job_parameters['storage']
+        data_parameters = job_parameters['data']
+        process_parameters = job_parameters['process']
+        model_parameters = job_parameters['model']
+
+        mlflow_parameters = storage_parameters['mlflow-parameters']
+        print('Setup MLflow')
+        mlflow_client = mlflow_setup_client(
+            mlflow_parameters = mlflow_parameters
+        )
+        
+        experiment_id = mlflow_get_or_create_experiment(
+            mlflow_client = mlflow_client,
+            name = mlflow_parameters['experiment-name']
+        )
+
+        run_id = mlflow_start_run(
+            mlflow_client = mlflow_client,
+            experiment_id = experiment_id, 
+            run_name = mlflow_parameters['run-name'], 
+            tags = mlflow_parameters['run-tags']
+        )
+        print('MLflow setup')
+        # This should be divided into batches based on worker number
+        print('Dividing work')
+        input_data = data_parameters['input']
+        given_worker_number = process_parameters['worker-number']
+
+        worker_batches = division_split_input(
+            job_input = input_data, 
+            num_workers = given_worker_number
+        )
+        print('Batch amount', len(worker_batches))
+        print(worker_batches)
+        print('Putting data into refs')
+        worker_batch_refs = []
+        for worker_batch in worker_batches:
+            worker_batch_refs.append(ray.put(worker_batch))
+        # We assume that actor number isn't ridiculus
+        given_actor_number = process_parameters['actor-number']
+        actor_number = min(given_actor_number,len(worker_batches))
+        
+        swift_parameters = storage_parameters['swift-parameters']
+        print('Creating ' + str(actor_number) + ' provider actors')
+        actor_refs = []
+        for i in range(0, actor_number):
+            actor_refs.append(Detector.remote(
+                swift_parameters = swift_parameters,
+                model_parameters = model_parameters
+            ))
+
+        print('Starting data collector tasks')
+        task_1_refs = [] 
+        worker_index = 1
+        actor_index = 0
+        for worker_batch_ref in worker_batch_refs:
+            actor_ref = actor_refs[actor_index]
+            task_1_refs.append(data_collector.remote(
+                worker_index = worker_index,
+                actor_index = actor_index + 1,
+                actor_ref = actor_ref,
+                storage_parameters = storage_parameters,
+                data_parameters = data_parameters,
+                task_batch = worker_batch_ref
+            ))
+            worker_index += 1
+            actor_index = (actor_index + 1) % actor_number
+        
+        print('Waiting data collector tasks')
+        collected_statistics = {} 
+        while len(task_1_refs):
+            done_task_1_refs, task_1_refs = ray.wait(task_1_refs)
+            for output_ref in done_task_1_refs:
+                collected_statistics.update(ray.get(output_ref))
+        
+        # remember that metrics only takes integers or floats
+        print('Logging metrics into MLflow')
+        flattened_statistics = flatten_nested_dict(
+            target_dict = collected_statistics,
+            parent_key = '',
+            seperator = '-'
+        )
+        
+        print(flattened_statistics)
+        mlflow_log_metrics(
+            mlflow_client = mlflow_client,
+            run_id = run_id, 
+            metrics = flattened_statistics, 
+            step = 0
+        ) 
+        print('Completing MLflow run')
+        mlflow_change_run_status(
+            mlflow_client = mlflow_client, 
+            run_id = run_id, 
+            status = 'FINISHED'
+        )
+
+        return True
+    except Exception as e:
+        print('das1 internal data analysis error', e)
+        return False
+
+if __name__ == "__main__":
+    print('Starting Ray job')
+    print('Python version is:' + str(sys.version))
+    check_packages = [
+        'ray',
+        'python-swiftclient',
+        'mlflow',
+        'pandas',
+        'pyarrow',
+        'fasttext',
+        'numpy',
+        'mlflow'
+    ]
+    for pkg_name in check_packages:
+        print(pkg_name,' version is ',version(pkg_name))
+    
+    print('Getting input')
+    job_parameters = json.loads(sys.argv[1])
+    #process_parameters = job_input['process']
+    #storage_parameters = job_input['storage']
+    #data_parameters = job_input['data']
+
+    print('Running DAS1 internal data analysis')
+    task_status = das1_internal_data_analysis(
+        job_parameters = job_parameters
+    )
+
+    print('Job success:' + str(task_status))
+    print('Ray job Complete')
---
+++ applications/pipelines/rag-coding-assistant/data-analysis-step-2/tasks/collector.py
@@ -0,0 +1,123 @@
+import ray
+import pickle
+import time as t
+from icebreaker.swift.setup import swift_setup_client
+from icebreaker.storage.management import object_storage_interaction
+from icebreaker.pd_stats.use import (
+    stats_pandas_content
+)
+from icebreaker.pyarrow.use import pyarrow_deserialize_dataframe
+
+@ray.remote(
+    num_cpus = 1,
+    memory = 0.2 * 1024 * 1024 * 1024
+)
+def data_collector(
+    worker_index: int,
+    actor_index: int,
+    actor_ref: any,
+    storage_parameters: any,
+    data_parameters: any,
+    task_batch: any
+) -> any:
+    start_time = t.time()
+    print('Task', worker_index, 'Actor', actor_index)
+    
+    swift_parameters = storage_parameters['swift-parameters']
+    print('Setting up swift client')
+    setup_swift_client = swift_setup_client(
+        swift_parameters = swift_parameters
+    )
+    print('Swift client setup') 
+
+    analysis_parameters = data_parameters['analysis-parameters']
+    data_storage = storage_parameters['data-storage']
+
+    language_column = analysis_parameters['language-column']    
+    format_column = analysis_parameters['format-column']
+
+    collected_stats = {}
+    provider_actor_refs = []
+    batch_index = 1
+    for batch_data in task_batch:
+        object_path = batch_data[0]
+        key_name = object_path.split('/')[-1].split('.')[0]
+        stored_dataset = object_storage_interaction(
+            storage_client = setup_swift_client,
+            lock_parameters = {},
+            lock_location = None,
+            parameters = {
+                'mode': 'get',
+                'bucket-target': data_storage['bucket-target'],
+                'bucket-prefix': data_storage['bucket-prefix'],
+                'bucket-user': data_storage['bucket-user'],
+                'debug-prints': True,
+                'object-name': 'root',
+                'path-replacers': {
+                    'name': object_path
+                },
+                'path-names': [],
+                'overwrite': True
+            },
+            object_data = None,
+            object_metadata = None
+        ) 
+        pandas_df = pyarrow_deserialize_dataframe(serialized_dataframe = stored_dataset[0])
+
+        collected_stats[key_name]['index'] = key_name.split('-')[-1]
+
+        amount = pandas_df.shape[0]
+        collected_stats[key_name]['amount'] = amount
+
+        # question-(type)-(method)
+        stats_pandas_content(
+            df = pandas_df,
+            target_column = 'question',
+            column_prefix = 'question',
+            collected_statistics = collected_stats[key_name]
+        )
+        # answer-(type)-(method)
+        stats_pandas_content(
+            df = pandas_df,
+            target_column = 'answer',
+            column_prefix = 'answer',
+            collected_statistics = collected_stats[key_name]
+        )
+
+        # Speaking language-(type)
+        text_input_ref_1 = ray.put(pandas_df[language_column])
+        provider_actor_refs.append(actor_ref.batch_fasttext_stats.remote(
+            worker_index = worker_index,
+            actor_index = actor_index,
+            batch_index = batch_index,
+            used_key = key_name,
+            text_input = text_input_ref_1,
+            analysis_parameters = analysis_parameters
+        ))
+
+        text_input_ref_2 = ray.put(pandas_df[format_column])
+        provider_actor_refs.append(actor_ref.batch_magika_stats.remote(
+            worker_index = worker_index,
+            actor_index = actor_index,
+            batch_index = batch_index,
+            used_key = key_name,
+            text_input = text_input_ref_2,
+            analysis_parameters = analysis_parameters
+        ))
+
+        batch_index += 1
+    
+    while len(provider_actor_refs):
+        done_actor_refs, provider_actor_refs = ray.wait(provider_actor_refs)
+        for output_ref in done_actor_refs: 
+            result = ray.get(output_ref)
+            batch_index = result['batch']
+            stats = result['stats']
+            key_name = result['key']
+            for stat_name, value in stats.items():
+                collected_stats[key_name][stat_name] = value
+    
+    end_time = t.time()
+    total_time = round(end_time-start_time,5)
+    print('Spent seconds', total_time)
+    return collected_stats
