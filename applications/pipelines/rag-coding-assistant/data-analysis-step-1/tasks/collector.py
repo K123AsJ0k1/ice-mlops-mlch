c43bb81ef8a533dacecc17abb2cc4975a050f0be
K123AsJ0k1 <niila.siilasjoki@gmail.com>
2026-05-25 11:45:38 +0300

    DA pipeline and DAS1 script fixes

--- applications/pipelines/rag-coding-assistant/data-analysis-step-1/tasks/collector.py
+++ applications/pipelines/rag-coding-assistant/data-analysis-step-1/tasks/collector.py
@@ -20,22 +20,23 @@ def data_collector(
     worker_index: int,
     actor_index: int,
     actor_ref: any,
-    storage_parameters: any,
-    data_parameters: any,
+    swift_parameters: any,
+    data_storage_parameters: any,
+    config_parameters: any,
     task_batch: any
 ) -> any:
     start_time = t.time()
     print('Task', worker_index, 'Actor', actor_index)
     
-    swift_parameters = storage_parameters['swift-parameters']
+    #swift_parameters = job_parameters['swift']
     print('Setting up swift client')
     setup_swift_client = swift_setup_client(
         swift_parameters = swift_parameters
     )
     print('Swift client setup') 
 
-    analysis_parameters = data_parameters['analysis-parameters']
-    data_storage = storage_parameters['data-storage']
+    analysis_parameters = config_parameters['analysis-parameters']
+    #data_storage = job_parameters['data-storage']
 
     language_column = analysis_parameters['language-column']
     
@@ -53,9 +54,9 @@ def data_collector(
             lock_location = None,
             parameters = {
                 'mode': 'get',
-                'bucket-target': data_storage['bucket-target'],
-                'bucket-prefix': data_storage['bucket-prefix'],
-                'bucket-user': data_storage['bucket-user'],
+                'bucket-target': data_storage_parameters['bucket-target'],
+                'bucket-prefix': data_storage_parameters['bucket-prefix'],
+                'bucket-user': data_storage_parameters['bucket-user'],
                 'debug-prints': True,
                 'object-name': 'root',
                 'path-replacers': {
--- applications/pipelines/rag-coding-assistant/data_analysis.py
+++ applications/pipelines/rag-coding-assistant/data_analysis.py
@@ -7,9 +7,9 @@ from kfp import dsl
     ]
 )
 def multi_submission_step(
-    storage_parameters: dict,
-    integration_parameters: dict,
-    process_parameters: dict,
+    storage: dict,
+    integration: dict,
+    process: dict,
     step_key: str
 ):
     import time as t
@@ -32,16 +32,16 @@ def multi_submission_step(
     
     # Creates connection to allas
     print('Storage parameters')
-    swift_parameters = storage_parameters['swift-parameters']
+    swift_parameters = storage['swift']
     print('Setting up swift client')
     setup_swift_client = swift_setup_client(
         swift_parameters = swift_parameters
     )
     print('Swift client setup')
-    local_cloud_cluster_yamls = integration_parameters['cluster-yamls']
-    step_parameters = process_parameters[step_key]
-    step_general_parameters = step_parameters['general-parameters']
-    step_cluster_parameters = step_parameters['cluster-parameters']
+    local_cloud_cluster_yamls = integration['cluster-yamls']
+    step_process_parameters = process[step_key]
+    step_general_parameters = step_process_parameters['general']
+    step_cluster_parameters = step_process_parameters['cluster']
     
     # Checks what clusters are available and then divides the work
     cluster_clients = ray_get_clients(
@@ -51,7 +51,7 @@ def multi_submission_step(
     print(cluster_clients)
     if 0 < len(cluster_clients):
         print('Clusters available')
-        code_storage = storage_parameters['code-storage']
+        code_storage = storage['code-storage']
         
         for cluster, details in step_cluster_parameters.items():
             cluster_job_runtime = details['job']['runtime']
@@ -61,8 +61,8 @@ def multi_submission_step(
                 storage_parameters = code_storage,
                 ray_runtime = cluster_job_runtime
             )
-            step_parameters['cluster-parameters'][cluster]['job']['runtime']['working_dir'] = job_directory
-            step_parameters['cluster-parameters'][cluster]['job']['runtime']['pip'] = job_requirements
+            step_process_parameters['cluster'][cluster]['job']['runtime']['working_dir'] = job_directory
+            step_process_parameters['cluster'][cluster]['job']['runtime']['pip'] = job_requirements
         
         # Splits the work
         print('Creating a cluster data distribution')
@@ -70,7 +70,7 @@ def multi_submission_step(
             ray_clusters = cluster_clients
         )
         print(formatted_clusters)
-        local_cloud_resource_weights = integration_parameters['resource-weights']
+        local_cloud_resource_weights = integration['resource-weights']
         local_cloud_cluster_weights = division_cluster_weights(
             resource_weights = local_cloud_resource_weights,
             formatted_clusters = formatted_clusters
@@ -78,8 +78,8 @@ def multi_submission_step(
 
         print(local_cloud_cluster_weights)
         if 0 < len(local_cloud_cluster_weights):
-            data_storage = storage_parameters['data-storage']
-            object_prefix = step_general_parameters['storage']['data-storage']['object-prefix']
+            data_storage = storage['data-storage']
+            object_prefix = step_general_parameters['data-storage']['object-prefix']
             dataset_tuple_list = data_list_objects(
                 storage_client = setup_swift_client,
                 storage_parameters = data_storage,
@@ -95,7 +95,7 @@ def multi_submission_step(
             cluster_job_ids = ray_multi_submit(
                 cluster_clients = cluster_clients,
                 cluster_inputs = clustered_dataset_paths,
-                step_parameters = step_parameters
+                step_parameters = step_process_parameters
             )
 
             job_logs = ray_multi_wait(
@@ -104,7 +104,7 @@ def multi_submission_step(
                 loop_wait = 5
             )
             
-            log_storage = storage_parameters['log-storage']
+            log_storage = storage['log-storage']
             ray_store_logs(
                 storage_client = setup_swift_client,
                 storage_parameters = log_storage,
@@ -121,23 +121,23 @@ def multi_submission_step(
     description = "Internal and external data analysis"
 )
 def data_analysis_pipeline(
-    storage_parameters: dict,
-    integration_parameters: dict,
-    process_parameters: dict
+    storage: dict,
+    integration: dict,
+    process: dict
 ):
-    #task_1 = multi_submission_step(
+    task_1 = multi_submission_step(
+        storage_parameters = storage,
+        integration_parameters = integration,
+        process_parameters = process,
+        step_key = 'step-1'
+    )
+
+    #task_2 = multi_submission_step(
     #    storage_parameters = storage_parameters,
     #    integration_parameters = integration_parameters,
     #    process_parameters = process_parameters,
-    #    step_key = 'step-1'
+    #    step_key = 'step-2'
     #)
-
-    task_2 = multi_submission_step(
-        storage_parameters = storage_parameters,
-        integration_parameters = integration_parameters,
-        process_parameters = process_parameters,
-        step_key = 'step-2'
-    )
 
     #task_2 = multi_submission_step(
     #    storage_parameters = storage_parameters,
