bad8d987f1797fd2bdce729bc2928475881a1045
K123AsJ0k1 <niila.siilasjoki@gmail.com>
2026-06-03 12:14:59 +0300

    Completed microcontroller

--- applications/integration/controller/setup_controller.py
+++ applications/integration/controller/setup_controller.py
@@ -8,15 +8,20 @@ from icebreaker.swift.setup import swift_get_parameters, swift_setup_client
 from icebreaker.swift.use import swift_get_object_content
 from icebreaker.misc.dict import check_dict_path, get_dict_value
 from icebreaker.docker.use import docker_manage_compose
+from icebreaker.logger.setup import logger_setup_configuration
 
 def setup_controller(
     input_path: str
 ):
-    print("Starting Ray-Chisel micro-controller")
+    logger = logger_setup_configuration(
+        logger_name = 'controller'
+    )
+    
+    logger.info("Starting Ray-Chisel micro-controller")
 
-    print('Getting input file')
+    logger.info('Getting input file')
     if not os.path.exists(input_path):
-        print(f"Error: File not found at {input_path}")
+        logger.info(f"Error: File not found at {input_path}")
         return False
 
     yaml_dict = {}
@@ -24,9 +29,9 @@ def setup_controller(
         yaml_dict = yaml.safe_load(f)
 
     env_path = yaml_dict['env_path']
-    print('Getting env file')
+    logger.info('Getting env file')
     if not os.path.exists(env_path):
-        print(f"Error: File not found at {env_path}")
+        logger.info(f"Error: File not found at {env_path}")
         return False
     
     env_dict = Config(RepositoryEnv(env_path))
@@ -41,10 +46,10 @@ def setup_controller(
     swift_auth_version = env_dict.get('SWIFT_AUTH_VERSION')
     swift_pre_auth_url = env_dict.get('SWIFT_PRE_AUTH_URL')
     
-    print('Creating swift parameters')
+    logger.info('Creating swift parameters')
     setup_swift_parameters = {}
     if 0 < len(swift_user) and 0 < len(swift_password):
-        print('Username and password exist')
+        logger.info('Username and password exist')
         setup_swift_parameters = swift_get_parameters(
             secret_parameters = {
                 'swift-auth-url': swift_auth_url,
@@ -57,7 +62,7 @@ def setup_controller(
             }
         )
     if 0 < len(swift_token):
-        print('Token exists')
+        logger.info('Token exists')
         setup_swift_parameters = {
             'pre-auth-token': swift_token,
             'pre-auth-url': swift_pre_auth_url,
@@ -67,7 +72,7 @@ def setup_controller(
             'project-domain-name': swift_domain_name,
         }
     
-    print('Setting up swift client')
+    logger.info('Setting up swift client')
     swift_client = swift_setup_client(
         swift_parameters = setup_swift_parameters
     )
@@ -82,7 +87,7 @@ def setup_controller(
     check_interval = yaml_dict['check_interval']
     current_state = "UNKNOWN"
     while True:
-        print('Getting cluster')
+        logger.info('Getting cluster')
         object_content = swift_get_object_content(
             swift_client = swift_client,
             bucket_name = object_bucket,
@@ -90,7 +95,7 @@ def setup_controller(
         )
         cluster_yamls = pickle.loads(object_content['data'])
 
-        print(f"Checking dict path: {controlled_path}")
+        logger.info(f"Checking dict path: {controlled_path}")
         path_exists = check_dict_path(
             target_dict = cluster_yamls,
             key_path = controlled_path,
@@ -109,7 +114,8 @@ def setup_controller(
         current_state = docker_manage_compose(
             file_path = compose_file_path,
             current_state = current_state,
-            wanted_state = cluster_dict['activate']
+            wanted_state = cluster_dict['activate'],
+            print_function = logger.info
         )
 
         time.sleep(check_interval)
