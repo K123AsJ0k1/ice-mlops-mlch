173d515340b69ca0a7c9bbbe6cbc8ecb0b6d9018
K123AsJ0k1 <niila.siilasjoki@gmail.com>
2026-06-10 12:31:48 +0300

    Package mlflow fixes

--- applications/packages/icebreaker/src/icebreaker/mlflow/use.py
+++ applications/packages/icebreaker/src/icebreaker/mlflow/use.py
@@ -33,7 +33,7 @@ def mlflow_log_metrics(
     step: int
 ) -> None:
     for key, val in metrics.items():
-        if not isinstance(val, list):
+        if not isinstance(val, list) or not isinstance(val, dict):
             mlflow_client.log_metric(
                 run_id = run_id, 
                 key = key, 
