1186533765f7eeda73ddceeffde9886fa9c21e88
K123AsJ0k1 <niila.siilasjoki@gmail.com>
2026-06-17 09:06:00 +0300

    Package swift application cred setup

--- applications/packages/icebreaker/src/icebreaker/swift/setup.py
+++ applications/packages/icebreaker/src/icebreaker/swift/setup.py
@@ -1,6 +1,6 @@
 def swift_client_check(
     storage_client: any
-) -> any:
+) -> bool:
     try:
         import swiftclient as sc
     except ImportError as e:
@@ -10,13 +10,17 @@ def swift_client_check(
 
 def swift_get_parameters(
     secret_parameters: any
-):
+) -> dict:
     try:
+        import time
         from keystoneauth1 import loading, session
+        from keystoneauth1.identity import v3
     except ImportError as e:
         raise ImportError("swift/setup failed to import", e)
 
     swift_auth_url = secret_parameters['swift-auth-url']
+    swift_application_credential_id = secret_parameters['swift-application-credential-id']
+    swift_application_credential_secret = secret_parameters['swift-application-credential-secret']
     swift_auth_version = secret_parameters['swift-auth-version']
     swift_user = secret_parameters['swift-user']
     swift_key = secret_parameters['swift-key']
@@ -25,22 +29,36 @@ def swift_get_parameters(
     swift_project_domain_name = secret_parameters['swift-domain-name']
     swift_pre_auth_url = secret_parameters['swift-pre-auth-url']
 
-    loader = loading.get_plugin_loader('password')
-    auth = loader.load_from_options(
-        auth_url = swift_auth_url,
-        username = swift_user,
-        password = swift_key,
-        project_name = swift_project_name,
-        user_domain_name = swift_user_domain_name,
-        project_domain_name = swift_project_domain_name
-    )
+    auth_plugin = None
+    if 0 < len(swift_application_credential_id) and 0 < len(swift_application_credential_secret):
+        print('Using application credentials for session')
+        auth_plugin = v3.ApplicationCredential(
+            auth_url = swift_auth_url,
+            application_credential_id = swift_application_credential_id,
+            application_credential_secret = swift_application_credential_secret
+        )
+    else:
+        print('Using user-password for session')
+        loader = loading.get_plugin_loader('password')
+        auth_plugin = loader.load_from_options(
+            auth_url = swift_auth_url,
+            username = swift_user,
+            password = swift_key,
+            project_name = swift_project_name,
+            user_domain_name = swift_user_domain_name,
+            project_domain_name = swift_project_domain_name
+        )
 
     keystone_session = session.Session(
-        auth = auth
+        auth = auth_plugin
     )
+    last_auth_time = time.time()
     swift_token = keystone_session.get_token()
     
     swift_parameters = {
+        'last-auth-time': last_auth_time,
+        'credential-id': str(swift_application_credential_id),
+        'credential-secret': str(swift_application_credential_secret),
         'pre-auth-token': str(swift_token),
         'pre-auth-url': str(swift_pre_auth_url),
         'auth-version': str(swift_auth_version),
@@ -71,3 +89,28 @@ def swift_setup_client(
     )
     return swift_client
 
+def swift_renew_client(
+    secret_parameters: any,
+    swift_client: any
+) -> any:
+    try:
+        import time
+    except ImportError as e:
+        raise ImportError("swift/setup failed to import", e)
+    # Tokens expire in 8 hours
+    client_refresh_interval = 4 * 60 * 60 
+    last_auth_time = secret_parameters['last-auth-time']
+    elapsed_time = time.time() - last_auth_time
+    new_swift_client = swift_client
+    if client_refresh_interval <= elapsed_time:
+        print('Creating new client')
+        new_swift_parameters = swift_get_parameters(
+            secret_parameters = secret_parameters
+        )
+
+        new_swift_client = swift_setup_client(
+            swift_parameters = new_swift_parameters
+        )
+    else:
+        print('Returning current client')
+    return new_swift_client
