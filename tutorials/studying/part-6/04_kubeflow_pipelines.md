f4b5f259c81fff508382990f7ed21dfab18efb35
K123AsJ0k1 <niila.siilasjoki@gmail.com>
2026-09-10 10:49:36 +0300

    Part-06-04 KFP complete

---
+++ tutorials/studying/part-6/04_kubeflow_pipelines.md
@@ -0,0 +1,130 @@
+---
+technologies: "Kubeflow Pipelines"
+category: "Choice and use of technology"
+difficulty: "Intermediate"
+---
+
+# Kubeflow Pipelines
+
+## Used material
+
+1. <span id="used-material-1"></span> [Kubeflow pip package](https://pypi.org/project/kfp/)
+
+2. <span id="used-material-2"></span> [Kubeflow docs](https://www.kubeflow.org/docs/)
+
+3. <span id="used-material-3"></span> [Getting Started with Kubeflow: A Complete Beginner’s Guide](https://kubezilla.io/getting-started-with-kubeflow-a-complete-beginners-guide/)
+
+4. <span id="used-material-4"></span> [Compose components into pipelines](https://www.kubeflow.org/docs/components/pipelines/user-guides/components/compose-components-into-pipelines/)
+
+5. <span id="used-material-5"></span> [Create, use, pass, and track ML artifacts](https://www.kubeflow.org/docs/components/pipelines/user-guides/data-handling/artifacts/)
+
+6. <span id="used-material-6"></span> [Control Flow](https://www.kubeflow.org/docs/components/pipelines/user-guides/core-functions/control-flow/)
+
+## Why use Kubeflow Pipelines?
+
+Kubeflow Pipelines (KFP) is widely used for the following reasons:
+
+- Provides an orchestration tool with integrated ML metadata tracking that is supported by many cloud providers, with some providing KFP SDK-managed cloud services (mature)
+
+- Easy-to-use Pythonic SDK that decouples the execution environment from the pipeline definition, with the steps of that pipeline run as isolated containers (abstracted)
+
+- Widely supports various frameworks and infrastructure due to containerization, with Kubernetes-provided plugins for customization that enable the creation of end-to-end MLOps pipelines (interoperable)
+
+These features make KFP the default pipeline platform for creating MLOps workflow pipelines that can be shared and utilize provided functions to handle orchestration that managed multiple Ray clusters running in local, cloud and HPC enviroments. 
+
+## How to use Kubeflow Pipelines?
+
+Assuming you set up the OSS MLOps platform using the OSS chapter and the network with the Istio chapter, you can start using KFP right away by opening http://kubeflow.oss:7001. In the dashboard, click Runs to see the list of pipeline runs. This will most likely be empty, which is why you should check the example picture below:
+
+![Figure 1](./images/kubeflow-dashboard-runs.png)
+
+Here, if you click one of the runs, you should get the following example pipeline graph:
+
+![Figure 2](./images/kubeflow-dashboard-graph.png)
+
+If you click a pipeline step, you should see a side view with options, input/output, task details, and logs. Be aware that during runs, these will take time to appear. From these options, input/output is useful for double-checking the given arguments, while logs enable you to see what happened during the run, such as the one seen in the example picture below:
+
+![Figure 3](./images/kubeflow-dashboard-logs.png)
+
+If you click root from the layers above the view and click the zoom button on the right side of for-loop-1, you'll get to see how many clusters were managed as seen in the following picture:
+
+![Figure 4](./images/kubeflow-dashboard-clusters.png)
+
+If you click root from the layers above the view and click the zoom button on the right side of for-loop-1, you'll get to see how many clusters were managed, as seen in the following picture:
+
+![Figure 5](./images/kubeflow-dashboard-cluster-step.png)
+
+With this, we can now monitor the progress of various pipelines and get their logs for later use. The pipelines shown in the images, which we will use later, are included in the example [Py file](./pipelines/cluster_pipeline.py). The code consists of the following steps |[(1)](#used-material-1), [(2)](#used-material-2), [(3)](#used-material-3), [(4)](#used-material-4), [(5)](#used-material-5), [(6)](#used-material-6)|:
+
+1. Cluster_setup_step
+    - Activates local, cloud, and HPC Ray clusters
+
+2. global_distribution_step
+    - Divides the object paths between available clusters, puts them into a KFP output directory artifact and creates a namedtuple for the output keys
+    
+3. cluster_orchestrator_step
+    - Uses the track key to load the output from directory artifact, downloads the Ray script and sends it to the target Ray cluster with parameters
+
+These steps enable us to create the following pipeline code:
+
+```
+@dsl.pipeline(
+    name = "cluster-pipeline",
+    description = "cluster pipeline"
+)
+def cluster_pipeline(
+    storage: dict,
+    integration: dict,
+    processing: dict
+):
+    
+    cluster_setup_task = cluster_setup_step(
+        storage = storage,
+        integration = integration
+    )
+    
+    global_distribution_task = global_distribution_step(
+        storage = storage,
+        integration = integration,
+        processing = processing
+    ).after(cluster_setup_task)
+    
+    with dsl.ParallelFor(global_distribution_task.outputs['track_keys']) as track_id:
+        current_task = cluster_orhestractor_step(
+            storage = storage,
+            integration = integration,
+            processing = processing,
+            tracks_input_dir = global_distribution_task.outputs['tracks_output_dir'],
+            track_id = track_id
+        )
+```
+
+The key components in this pipeline are .after() to ensure sequential step processing, an output directory artifact to store divided inputs, a namedtuple to enable ParallelFor to create cluster steps, and using the output directory artifact with the track id to load the correct input paths.
+
+With this, we have a generalized pipeline code we can use with small edits to activate Ray clusters, run the Ray scripts we define in the steps of a pipeline dictionary, and shut down Ray clusters. The pipeline code can be submitted in a Jupyter notebook in the following way:
+
+```
+import kfp
+KFP_ENDPOINT = "http://kubeflow.oss:7001"
+kfp_client = kfp.Client(host=KFP_ENDPOINT)
+---
+%run ice-mlops-mlch/tutorials/studying/part-6/pipelines/cluster_pipeline.py
+---
+from icebreaker.kubeflow.use import kubeflow_manage_run
+data_analysis_pipeline_time = kubeflow_manage_run(
+    storage_client = workflow_swift_client,
+    kfp_client = kfp_client,
+    pipeline_function = cluster_pipeline,
+    run_name = pipeline_run_name,
+    experiment_name = pipeline_experiment_name,
+    pipeline_arguments = cluster_pipeline_arguments,
+    cache_steps = False,
+    time_update_object = time_object_name_update,
+    loop_amount = 20000,
+    loop_wait = 10
+)
+```
+
+Together, these pieces ensure Jupyter Notebooks can collect pipeline inputs, load the pipeline function, submit it to the OSS MLOps platform, monitor its progress via available dashboards, and interact with the resulting pipeline artifacts. We will provide such a demonstration later.
+
+---
---
+++ tutorials/studying/part-6/pipelines/cluster_pipeline.py
@@ -0,0 +1,279 @@
+from kfp import dsl
+from typing import NamedTuple
+from kfp.dsl import Output, Input, Dataset
+
+@dsl.component(
+    base_image = "python:3.12.3",
+    packages_to_install = [
+        "icebreaker[swift, data] @ git+https://github.com/K123AsJ0k1/ice-mlops-mlch.git@main#subdirectory=applications/packages/icebreaker"
+    ]
+)
+def cluster_setup_step(
+    storage: dict,
+    integration: dict
+):
+    import time as t
+    start_time = t.time()
+
+    from icebreaker.swift.setup import swift_setup_client 
+    from icebreaker.objects.use import objects_store_data 
+    from icebreaker.misc.time import time_run_update
+
+    swift_parameters = storage['swift']
+    print('Setting up swift client')
+    setup_swift_client = swift_setup_client(
+        swift_parameters = swift_parameters
+    )
+    code_storage = storage['code-storage']
+    cluster_name = integration['cluster-name']
+    cluster_yamls = integration['cluster-yamls']
+    object_name = cluster_name + '.pkl'
+    print('Storing cluster yamls')
+    store_status = objects_store_data(
+        swift_client = setup_swift_client,
+        storage_parameters = {
+            'bucket-target': code_storage['bucket-target'],
+            'bucket-prefix': code_storage['bucket-prefix'],
+            'bucket-user': code_storage['bucket-user'],
+            'object-name': 'mana',
+            'object-serialization': 'pickle',
+            'path-replacers': {
+                'name': object_name
+            },
+            'path-names': [],
+            'debug-prints': True,
+            'lock-parameters': {},
+            'lock-location': None,
+            'overwrite': True
+        },
+        object_data = cluster_yamls,
+        object_metadata = {}
+    )
+    
+    end_time = t.time()
+
+    time_storage_parameters = storage['time-storage']
+    time_object_name = time_storage_parameters['object-name']
+    time_name = 'cluster-setup-step' 
+    time_stored_1, time_index_1, time_name_1 = time_run_update(
+        storage_client = setup_swift_client,
+        storage_parameters = time_storage_parameters,
+        object_name = time_object_name,
+        time_name = time_name,
+        start_time = start_time,
+        end_time = end_time,
+        time_index = -1
+    )
+    total_time = round(end_time-start_time,5)
+    print('Spent seconds', total_time)
+    
+@dsl.component(
+    base_image = "python:3.12.3",
+    packages_to_install = [
+        "icebreaker[swift, data, ray, pararellism] @ git+https://github.com/K123AsJ0k1/ice-mlops-mlch.git@main#subdirectory=applications/packages/icebreaker"
+    ]
+)
+def global_distribution_step(
+    storage: dict,
+    integration: dict,
+    processing: dict,
+    tracks_output_dir: Output[Dataset]
+) -> NamedTuple("Output", track_keys = list):
+    import time as t
+    start_time = t.time()
+    
+    import os
+    import json
+    from icebreaker.swift.setup import swift_setup_client 
+    from icebreaker.pararellism.distribute import distribute_step_inputs
+    from icebreaker.misc.time import time_run_update
+    from collections import namedtuple
+
+    swift_parameters = storage['swift']
+    print('Setting up swift client')
+    setup_swift_client = swift_setup_client(
+        swift_parameters = swift_parameters
+    )
+
+    track_inputs = distribute_step_inputs(
+        storage_client = setup_swift_client,
+        storage = storage,
+        integration = integration,
+        processing = processing
+    )
+
+    os.makedirs(tracks_output_dir.path, exist_ok=True)
+    track_ids = []
+    for index, track_payload in enumerate(track_inputs):
+        track_id = f"track_{index}"
+        track_ids.append(track_id)
+        file_path = os.path.join(tracks_output_dir.path, f"{track_id}.json")
+        with open(file_path, "w") as f:
+            json.dump(track_payload, f)
+
+    time_storage_parameters = storage['time-storage']
+    time_object_name = time_storage_parameters['object-name']
+    time_name = 'global-distribution-step'
+    end_time = t.time()
+    time_stored_1, time_index_1, time_name_1 = time_run_update(
+        storage_client = setup_swift_client,
+        storage_parameters = time_storage_parameters,
+        object_name = time_object_name,
+        time_name = time_name,
+        start_time = start_time,
+        end_time = end_time,
+        time_index = -1
+    )
+    total_time = round(end_time-start_time,5)
+    print('Spent seconds', total_time)
+
+    output = namedtuple("Output", ["track_keys"])
+    
+    return output(
+        track_keys = track_ids
+    )
+
+@dsl.component(
+    base_image = "python:3.12.3",
+    packages_to_install = [
+        "icebreaker[swift, data, ray] @ git+https://github.com/K123AsJ0k1/ice-mlops-mlch.git@main#subdirectory=applications/packages/icebreaker"
+    ]
+)
+def cluster_orhestractor_step(
+    storage: dict,
+    integration: dict,
+    processing: dict,
+    tracks_input_dir: Input[Dataset],
+    track_id: str    
+):
+    import time as t
+    import os
+    import json
+    from icebreaker.swift.setup import swift_setup_client 
+    from icebreaker.ray.setup import ray_download_job
+    from icebreaker.ray.use import ray_get_clients, ray_parallel_submit, ray_parallel_wait, ray_store_logs
+    from icebreaker.misc.time import time_run_update
+
+    start_time = t.time()
+
+    print('Storage parameters')
+    swift_parameters = storage['swift']
+    print('Setting up swift client')
+    work_swift_client = swift_setup_client(
+        swift_parameters = swift_parameters
+    )
+    cluster_name = 'default'
+   
+    file_path = os.path.join(tracks_input_dir.path, f"{track_id}.json")
+    with open(file_path, "r") as f:
+        track_data_list = json.load(f)
+        
+    if not isinstance(track_data_list, list):
+        track_data_list = [track_data_list]
+
+    if 0 < len(track_data_list):
+        code_storage = storage['code-storage']
+        cluster_yamls = integration['cluster-yamls']
+        print(f'Track has {len(track_data_list)} inputs')
+        for step_data in track_data_list:
+            cluster_step = step_data['cluster_step']
+            cluster_name = step_data['cluster_name']
+            step_input = step_data['cluster_input']
+            cluster_inputs = {
+                cluster_name: step_input 
+            }
+            print(f'Checking track step {cluster_step} in cluster {cluster_name} with input size {len(step_input)}')
+            
+            if cluster_step in processing:
+                step_processing_parameters = processing[cluster_step]
+                print('Step exists')
+                if cluster_name in step_processing_parameters['cluster']:
+                    cluster_parameters = step_processing_parameters['cluster']
+                    cluster_clients = ray_get_clients(
+                        configured_clusters = cluster_yamls,
+                        cluster_parameters = cluster_parameters,
+                        cluster_filter = [
+                            cluster_name
+                        ]
+                    ) 
+                    if 0 < len(cluster_clients):
+                        print('Cluster exists')
+                        cluster_job_runtime = cluster_parameters[cluster_name]['job']['runtime']
+                        job_directory, job_requirements = ray_download_job(
+                            storage_client = work_swift_client,
+                            storage_parameters = code_storage,
+                            ray_runtime = cluster_job_runtime
+                        )
+                        step_processing_parameters['cluster'][cluster_name]['job']['runtime']['working_dir'] = job_directory
+                        step_processing_parameters['cluster'][cluster_name]['job']['runtime']['pip'] = job_requirements
+                        print('Submitting job')
+                        cluster_job_ids = ray_parallel_submit(
+                            cluster_clients = cluster_clients,
+                            cluster_inputs = cluster_inputs,
+                            step_name = cluster_step,
+                            step_parameters = step_processing_parameters
+                        )
+                        
+                        job_logs = ray_parallel_wait(
+                            cluster_job_ids = cluster_job_ids,
+                            multi_loop_amount = integration['multi-loop-amount'],
+                            multi_loop_wait = integration['multi-loop-wait'],
+                            job_loop_amount = integration['job-loop-amount'],
+                            job_loop_wait = integration['job-loop-wait']
+                        )
+                        
+                        log_storage = storage['log-storage']
+                        log_object_prefix = cluster_name + '-' + cluster_step
+                        ray_store_logs(
+                            storage_client = work_swift_client,
+                            storage_parameters = log_storage,
+                            job_directory = job_directory,
+                            job_logs = job_logs,
+                            object_prefix = log_object_prefix
+                        )
+        
+    end_time = t.time()
+    time_storage_parameters = storage['time-storage']
+    time_object_name = time_storage_parameters['object-name']
+    time_name = 'cluster-orhestractor-step-' + cluster_name 
+    time_stored_1, time_index_1, time_name_1 = time_run_update(
+        storage_client = work_swift_client,
+        storage_parameters = time_storage_parameters,
+        object_name = time_object_name,
+        time_name = time_name,
+        start_time = start_time,
+        end_time = end_time,
+        time_index = -1
+    )
+    total_time = round(end_time-start_time,5)
+    print('Spent seconds', total_time)
+
+@dsl.pipeline(
+    name = "cluster-pipeline",
+    description = "cluster pipeline"
+)
+def cluster_pipeline(
+    storage: dict,
+    integration: dict,
+    processing: dict
+):
+    
+    cluster_setup_task = cluster_setup_step(
+        storage = storage,
+        integration = integration
+    )
+    
+    global_distribution_task = global_distribution_step(
+        storage = storage,
+        integration = integration,
+        processing = processing
+    ).after(cluster_setup_task)
+    
+    with dsl.ParallelFor(global_distribution_task.outputs['track_keys']) as track_id:
+        current_task = cluster_orhestractor_step(
+            storage = storage,
+            integration = integration,
+            processing = processing,
+            tracks_input_dir = global_distribution_task.outputs['tracks_output_dir'],
+            track_id = track_id
+        )
